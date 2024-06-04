85396b33212097465fac16aad7d18c5bf184bb24
smilejakdu <ash982416@gmail.com>
2024-06-04 23:26:16 +0900

    mentoring 3 weeks

--- Week2/terraform-backend/terraform-backend.tf
+++ Week2/terraform-backend/terraform-backend.tf
@@ -1,13 +1,15 @@
 resource "aws_s3_bucket" "test-s3-tf-state" {
+  // AWS S3 버킷을 생성하는 리소스를 정의합니다.
 
   // 버킷 이름
+  // 생성할 S3 버킷의 이름을 "ash-test-s3-bucket-testuser"로 설정합니다.
   bucket = "ash-test-s3-bucket-testuser"
 
   // 태그명
+  // S3 버킷에 "Name"이라는 키와 "ash-test-s3-bucket-testuser"라는 값을 가진 태그를 추가합니다.
   tags = {
     "Name" = "ash-test-s3-bucket-testuser"
   }
-  
 }
 
 resource "aws_dynamodb_table" "test-ddb-tflock-state" {
